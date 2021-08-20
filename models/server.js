065e4f1c9e2840e74cc7bbf27530b856533b18f7
15ivan15 <ivanmolina15xd1@gmail.com>
2021-08-19 22:15:03 -0500

    puerto

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ class Server {
 
     constructor() {
         this.app  = express();
-        this.port = config.get('PORT');
+        this.port = process.env.PORT || config.get('PORT');
         this.authPath = '/api/auth';
         this.usuariosPath = '/api/usuarios';
 
